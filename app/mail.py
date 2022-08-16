916a5cc80493d23f68c52bbe494a71d7ce26e397
Mario Roberto Gómez <mrgomez.dev@gmail.com>
2022-08-16 13:14:06 -0600

    flask run chachito feliz en html

--- app/mail.py
+++ app/mail.py
@@ -1,9 +1,9 @@
 from flask import (
-    Blueprint
+    Blueprint, render_template
 )
 
 bp = Blueprint('mail', __name__, url_prefix="/")
 
 @bp.route('/', methods=['GET'])
 def index():
-    return 'lala'
+    return render_template('mails/index.html')
