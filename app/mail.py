0a6dc5f2f63319f64c739827277bc204548d4d0f
Mario Roberto Gómez <mrgomez.dev@gmail.com>
2022-08-16 16:13:24 -0600

    flask run connection mysql print terminal ok

--- app/mail.py
+++ app/mail.py
@@ -2,8 +2,16 @@ from flask import (
     Blueprint, render_template
 )
 
+from app.db import get_db
+
 bp = Blueprint('mail', __name__, url_prefix="/")
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('mails/index.html')
+    db, c = get_db()
+
+    c.execute("SELECT * FROM email")
+    mails = c.fetchall()
+
+    # print(mails) #print para mostrar correos en la terminal flask run
+    return render_template('mails/index.html', mails=mails)
