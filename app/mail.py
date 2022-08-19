4a970cb43909ca3cde58d5cc92dbc1e04bfa7d20
Mario Roberto Gómez <mrgomez.dev@gmail.com>
2022-08-19 14:50:17 -0600

    vadilacion de campos de formulario ok - flask run ok

--- app/mail.py
+++ app/mail.py
@@ -1,6 +1,6 @@
 #from crypt import methods  #La importacion de este modulo causa conflicto porque no es soportado por windows
 from flask import (
-    Blueprint, render_template, request
+    Blueprint, render_template, request, flash
 )
 
 from app.db import get_db
@@ -26,6 +26,25 @@ def create():
         email = request.form.get('email')
         subject = request.form.get('subject')
         content = request.form.get('content')
+        errors = []
 
-        print(name, phone, email, subject, content)
+        if not name:
+            errors.append("El Nombre es obligatorio")
+        if not phone:
+            errors.append("El Número de Telefono o WhatsApp es obligatorio")
+        if not email:
+            errors.append("Email es obligatorio")
+        if not subject:
+            errors.append("El Asunto del correo es obligatorio")
+        if content:
+            errors.append("El Contenido del correo es obligatorio")
+
+        #print(name, phone, email, subject, content)
+        #print(errors)
+
+        if len(errors) == 0:
+            pass
+        else:
+            for error in errors:
+                flash(error)
     return render_template('mails/create.html')
