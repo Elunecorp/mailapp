c47752b8dc47486de7f9d98cb48c5b527c2b01d3
Mario Roberto Gómez <mrgomez.dev@gmail.com>
2022-08-19 15:09:27 -0600

    insert valores en bd ok - validacion de campos ok - flask run

--- app/mail.py
+++ app/mail.py
@@ -1,6 +1,7 @@
 #from crypt import methods  #La importacion de este modulo causa conflicto porque no es soportado por windows
+from contextlib import redirect_stderr
 from flask import (
-    Blueprint, render_template, request, flash
+    Blueprint, render_template, request, flash, url_for, redirect
 )
 
 from app.db import get_db
@@ -21,29 +22,33 @@ def index():
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        name = request.form.get('name')
+        #name = request.form.get('name')
         phone = request.form.get('phone')
         email = request.form.get('email')
         subject = request.form.get('subject')
         content = request.form.get('content')
         errors = []
 
-        if not name:
-            errors.append("El Nombre es obligatorio")
+        # if not name:
+        #     errors.append("El Nombre es obligatorio")
         if not phone:
             errors.append("El Número de Telefono o WhatsApp es obligatorio")
         if not email:
             errors.append("Email es obligatorio")
         if not subject:
             errors.append("El Asunto del correo es obligatorio")
-        if content:
+        if not content:
             errors.append("El Contenido del correo es obligatorio")
 
         #print(name, phone, email, subject, content)
         #print(errors)
 
         if len(errors) == 0:
-            pass
+            db, c = get_db()
+            c.execute("INSERT INTO email (phone, email, subject, content) VALUES (%s, %s, %s, %s)", (phone, email, subject, content))
+            db.commit()
+
+            return redirect(url_for('mail.index'))
         else:
             for error in errors:
                 flash(error)
