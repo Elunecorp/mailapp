7a7c7946e701fc2910cad27085edad4033cf6477
Mario Roberto Gómez <mrgomez.dev@gmail.com>
2022-08-18 18:01:03 -0600

    correccion de error en create.html campo name - prueba en console print ok - flask run

--- app/mail.py
+++ app/mail.py
@@ -1,6 +1,6 @@
 #from crypt import methods  #La importacion de este modulo causa conflicto porque no es soportado por windows
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request
 )
 
 from app.db import get_db
@@ -20,4 +20,12 @@ def index():
 
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        phone = request.form.get('phone')
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+
+        print(name, phone, email, subject, content)
     return render_template('mails/create.html')
