02890145012d742529f27c2e761e86d6ad2be3b9
Mario Roberto Gómez <mrgomez.dev@gmail.com>
2022-08-20 11:35:21 -0600

    correccion de errores - flask run ok

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Desktop\MyPythonAppProject\Mailer\venv
+set VIRTUAL_ENV=D:\Desktop\MyPythonAppProject\mailapp\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
